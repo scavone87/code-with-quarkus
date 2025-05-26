d8befe21bb7701e0516019bf029fc7ba93765d80
quarkusio <no-reply@quarkus.io>
2025-05-26 20:04:51 +0000

    Initial commit

---
+++ src/test/java/it/scavone87/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package it.scavone87;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
